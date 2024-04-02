48a3c31fbc840f1ec9d36c7d5d6f334e88fd2d87
Abhishek Raina <abhishek.raina.uit@gmail.com>
2024-04-02 22:52:12 +0530

    Extracted the checksession widget and placed it in the checkSession.dart file for code readability.

--- lib/main.dart
+++ lib/main.dart
@@ -6,34 +6,6 @@ import 'package:chitchat_02/screens/registration_screen.dart';
 import 'package:chitchat_02/screens/chat_screen.dart';
 import 'package:chitchat_02/Contollers/checkSession.dart';
 
-
-// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-// import 'package:firebase_performance/firebase_performance.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-
-
-// void configureFirebaseLogging() {
-//   FirebaseCrashlytics.instance.setCustomKey('App', 'Chitchat');
-//   FirebaseCrashlytics.instance.setCustomKey('Version', '1.0.0');
-//   FirebaseCrashlytics.instance.setCustomKey('Platform', 'Android');
-//   FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-//   FirebasePerformance.instance.setPerformanceCollectionEnabled(true);
-//   FirebaseFirestore.instance.settings = const Settings(
-//     persistenceEnabled: false,
-//   );
-//   Firebase.initializeApp();
-// }
-//
-// FirebaseOptions getFirebaseOptions() {
-//   return const FirebaseOptions(
-//     appId: 'YOUR_APP_ID',
-//     apiKey: 'YOUR_API_KEY',
-//     authDomain: 'YOUR_AUTH_DOMAIN',
-//     messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-//     projectId: 'YOUR_PROJECT_ID',
-//   );
-// }
-
 void main() {
 
   WidgetsFlutterBinding.ensureInitialized();
@@ -65,34 +37,4 @@ class chitchat_02 extends StatelessWidget {
   }
 }
 
-class CheckSession extends StatefulWidget {
-
-  static const String id = 'CheckSession';
-  const CheckSession({super.key});
-
-  @override
-  State<CheckSession> createState() => _CheckSessionState();
-}
-
-class _CheckSessionState extends State<CheckSession> {
-  @override
-  void initState() {
-    checkSessions().then((value) => {
-      if(value){
-        Navigator.pushReplacementNamed(context, ChatScreen.id),
-      }
-      else{
-        Navigator.restorablePopAndPushNamed(context, WelcomeScreen.id),
-      }
-    });
-
-    super.initState();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
-    );
-  }
-}
 
