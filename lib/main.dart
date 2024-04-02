698850b1e1f13fda629024f19f62c0ad901f8dfa
Abhishek Raina <abhishek.raina.uit@gmail.com>
2024-04-02 22:47:10 +0530

    Made a new Widget which helps in checking weather the user is logged in or not.

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,43 @@
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:chitchat_02/screens/welcome_screen.dart';
 import 'package:chitchat_02/screens/login_screen.dart';
 import 'package:chitchat_02/screens/registration_screen.dart';
 import 'package:chitchat_02/screens/chat_screen.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:firebase_performance/firebase_performance.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chitchat_02/Contollers/checkSession.dart';
 
 
-void configureFirebaseLogging() {
-  FirebaseCrashlytics.instance.setCustomKey('App', 'Chitchat');
-  FirebaseCrashlytics.instance.setCustomKey('Version', '1.0.0');
-  FirebaseCrashlytics.instance.setCustomKey('Platform', 'Android');
-  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-  FirebasePerformance.instance.setPerformanceCollectionEnabled(true);
-  FirebaseFirestore.instance.settings = const Settings(
-    persistenceEnabled: false,
-  );
-  Firebase.initializeApp();
-}
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+// import 'package:firebase_performance/firebase_performance.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-FirebaseOptions getFirebaseOptions() {
-  return const FirebaseOptions(
-    appId: 'YOUR_APP_ID',
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-  );
-}
-void main() async{
+
+// void configureFirebaseLogging() {
+//   FirebaseCrashlytics.instance.setCustomKey('App', 'Chitchat');
+//   FirebaseCrashlytics.instance.setCustomKey('Version', '1.0.0');
+//   FirebaseCrashlytics.instance.setCustomKey('Platform', 'Android');
+//   FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+//   FirebasePerformance.instance.setPerformanceCollectionEnabled(true);
+//   FirebaseFirestore.instance.settings = const Settings(
+//     persistenceEnabled: false,
+//   );
+//   Firebase.initializeApp();
+// }
+//
+// FirebaseOptions getFirebaseOptions() {
+//   return const FirebaseOptions(
+//     appId: 'YOUR_APP_ID',
+//     apiKey: 'YOUR_API_KEY',
+//     authDomain: 'YOUR_AUTH_DOMAIN',
+//     messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+//     projectId: 'YOUR_PROJECT_ID',
+//   );
+// }
+
+void main() {
+
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(const chitchat_02());
 }
 
@@ -47,14 +52,47 @@ class chitchat_02 extends StatelessWidget {
         ),
       ),
       // home: const WelcomeScreen(), // anyscreen in home open first when the app is started
-      initialRoute: WelcomeScreen.id, // similar to home.
+      initialRoute: CheckSession.id, // similar to home.
       routes: {
         ChatScreen.id:(context) =>  const ChatScreen(),
         LoginScreen.id:(context) =>  const LoginScreen(),
         RegistrationScreen.id:(context) =>  const RegistrationScreen(),
         WelcomeScreen.id:(context) =>  const WelcomeScreen(),
+        CheckSession.id:(context) => const CheckSession(),
 
       },
     );
   }
 }
+
+class CheckSession extends StatefulWidget {
+
+  static const String id = 'CheckSession';
+  const CheckSession({super.key});
+
+  @override
+  State<CheckSession> createState() => _CheckSessionState();
+}
+
+class _CheckSessionState extends State<CheckSession> {
+  @override
+  void initState() {
+    checkSessions().then((value) => {
+      if(value){
+        Navigator.pushReplacementNamed(context, ChatScreen.id),
+      }
+      else{
+        Navigator.restorablePopAndPushNamed(context, WelcomeScreen.id),
+      }
+    });
+
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
+
