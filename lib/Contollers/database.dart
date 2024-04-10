75f9833c2c810a7b76838e2b87bf9ae8c248b268
Abhishek Raina <abhishek.raina.uit@gmail.com>
2024-04-10 22:38:52 +0530

    Replaced the database object fromm  database.dart to checkSession.dart file to avoid calling appwrite client multiple times.

--- lib/Contollers/database.dart
+++ lib/Contollers/database.dart
@@ -1,20 +1,5 @@
-import 'package:appwrite/appwrite.dart';
 
-void main() async {
-  final client = Client()
-      .setEndpoint('https://cloud.appwrite.io/v1')
-      .setProject('<6603843572662569be9e>');
 
-  final databases = Databases(client);
 
-  try {
-    final document = databases.createDocument(
-        databaseId: '<66065cf55547546538d9>',
-        collectionId: '<66065d65702be24e26aa>',
-        documentId: ID.unique(),
-        data: { "title": "Hamlet" }
-    );
-  } on AppwriteException catch(e) {
-    print(e);
-  }
-}
+
+
